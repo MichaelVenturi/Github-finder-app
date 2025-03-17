41c63e1f7c8c9dfa884c240d2bb5d01089bd6cf3
MichaelVenturi <michaelventuri58@aol.com>
2025-03-17 16:05:16 -0400

    added tooltips to repo badges

--- src/components/repos/RepoItem.jsx
+++ src/components/repos/RepoItem.jsx
@@ -13,17 +13,29 @@ const RepoItem = ({ repo }) => {
         </h3>
         <p className="mb-3">{description}</p>
         <div>
-          <div className="mr-2 badge badge-soft badge-info badge-lg">
+          <div className="group mr-2 badge badge-soft badge-info badge-lg">
             <FaEye className="mr-2" /> {watchers_count}
+            <span className="absolute mt-12 scale-0 badge badge-ghost badge-xs text-gray-400/60 transition-all group-hover:scale-100">
+              watchers
+            </span>
           </div>
-          <div className="mr-2 badge badge-soft badge-success badge-lg">
+          <div className="group mr-2 badge badge-soft badge-success badge-lg">
             <FaStar className="mr-2" /> {stargazers_count}
+            <span className="absolute mt-12 scale-0 badge badge-ghost badge-xs text-gray-400/60 transition-all group-hover:scale-100">
+              stargazers
+            </span>
           </div>
-          <div className="mr-2 badge badge-soft badge-error badge-lg">
+          <div className="group mr-2 badge badge-soft badge-error badge-lg">
             <FaInfo className="mr-2" /> {open_issues}
+            <span className="absolute mt-12 scale-0 badge badge-ghost badge-xs text-gray-400/60 transition-all group-hover:scale-100">
+              open issues
+            </span>
           </div>
-          <div className="mr-2 badge badge-soft badge-warning badge-lg">
+          <div className="group mr-2 badge badge-soft badge-warning badge-lg">
             <FaUtensils className="mr-2" /> {forks}
+            <span className="absolute mt-12 scale-0 badge badge-ghost badge-xs text-gray-400/60 transition-all group-hover:scale-100">
+              forks
+            </span>
           </div>
         </div>
       </div>
